412735a988d8d1e53c52fbd6554def60c7d53f6d
duc96 <duc96>
2020-04-07 09:52:12 +0700

    ổn

--- app/src/main/java/vn/com/duan1/coffeemanagement/Adapter/SanPhamAdapter.java
+++ app/src/main/java/vn/com/duan1/coffeemanagement/Adapter/SanPhamAdapter.java
@@ -1,9 +1,12 @@
 package vn.com.duan1.coffeemanagement.Adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,12 +14,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
+import vn.com.duan1.coffeemanagement.DAO.SanPhamDAO;
 import vn.com.duan1.coffeemanagement.DataModel.SanPham;
 import vn.com.duan1.coffeemanagement.Menu_item.DrinkFragment;
 import vn.com.duan1.coffeemanagement.R;
 
+import static vn.com.duan1.coffeemanagement.MainActivity.idAfterLogin;
+
 public class SanPhamAdapter extends RecyclerView.Adapter<SanPhamAdapter.SanPhamViewHolder> {
 
+    SanPhamDAO sanPhamDAO;
     private ArrayList<SanPham> sanPhams;
     private Context context;
     DrinkFragment fr;
@@ -35,13 +42,43 @@ public class SanPhamAdapter extends RecyclerView.Adapter<SanPhamAdapter.SanPhamV
     }
 
     @Override
-    public void onBindViewHolder(@NonNull SanPhamViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull SanPhamViewHolder holder, final int position) {
 
-        SanPham sp = sanPhams.get(position);
+        sanPhamDAO = new SanPhamDAO(context);
+        final SanPham sp = sanPhams.get(position);
+
+        if(idAfterLogin.substring(0,2).equals("nv")){
+            holder.iv_xoa.setEnabled(false);
+            holder.iv_xoa.setVisibility(View.GONE);
+        }
 
         holder.ten.setText(sp.getTenSP());
         holder.mota.setText(sp.getMota());
         holder.gia.setText(sp.getGiaSP()+"");
+
+        holder.iv_xoa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Xóa sản phẩm")
+                        .setPositiveButton("Xóa", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                sanPhamDAO.xoaSanPham(sp);
+                                sanPhams.remove(position);
+                                notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+                builder.show();
+            }
+        });
     }
 
     @Override
@@ -53,12 +90,14 @@ public class SanPhamAdapter extends RecyclerView.Adapter<SanPhamAdapter.SanPhamV
         private TextView ten;
         private TextView mota;
         private TextView gia;
+        private ImageView iv_xoa;
 
         public SanPhamViewHolder(View itemView) {
             super(itemView);
             this.ten = itemView.findViewById(R.id.tv_ItemName);
             this.mota = itemView.findViewById(R.id.tv_ItemDescribe);
             this.gia = itemView.findViewById(R.id.tv_ItemPrice);
+            this.iv_xoa = itemView.findViewById(R.id.iv_DeleteItem);
         }
     }
 }
